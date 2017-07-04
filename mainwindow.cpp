8e4cee890f8800e9723b6dd8510ef2266735dc30
Fxlipe115 <felipegraeff@hotmail.com>
2017-07-03 21:52:32 -0300

    Popula lista e combobox Popula a lista com as colunas do arquivo aberto e coloca as colunas selecionadas na combobox.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,12 +2,24 @@
 #include "ui_mainwindow.h"
 #include "tablewindow.hpp"
 #include <QFileDialog>
+#include <QItemSelection>
 
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent),
   ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
+
+  listModel = new QStringListModel(this);
+  comboModel = new QStringListModel(this);
+
+  ui->listView_columns->setModel(listModel);
+  ui->comboBox->setModel(comboModel);
+
+  connect(ui->listView_columns->selectionModel(),
+        SIGNAL(selectionChanged(QItemSelection,QItemSelection)),
+        this, SLOT(selection_changed_handler(QItemSelection)));
+
 }
 
 MainWindow::~MainWindow()
@@ -29,6 +41,22 @@ void MainWindow::on_actionAbrir_triggered()
   QString fileName = QFileDialog::getOpenFileName(this, tr("Abrir Arquivo"),
                                                   "./",
                                                   tr("Arquivos CSV (*.csv)"));
+
+  if(fileName != nullptr)
+  {
+    QFile f(fileName);
+    if(f.open(QIODevice::ReadOnly))
+    {
+      QByteArray line = f.readLine().trimmed();
+      QList<QByteArray> array = line.split(';');
+      QStringList list;
+      for(auto& elem : array)
+      {
+        list << elem;
+      }
+      listModel->setStringList(list);
+    }
+  }
 }
 
 void MainWindow::on_actionSair_triggered()
@@ -40,4 +68,16 @@ void MainWindow::on_actionSobre_triggered()
 {
   //TODO
   //criar janela 'Sobre'
+}
+
+void MainWindow::selection_changed_handler(const QItemSelection& selection)
+{
+  QModelIndexList indexes = ui->listView_columns->selectionModel()->selectedIndexes();
+  QStringList list;
+  for(auto& i : indexes)
+  {
+    list << i.data(Qt::DisplayRole).toString();
+  }
+  comboModel->setStringList(list);
+  ui->comboBox->setCurrentIndex(0);
 }
