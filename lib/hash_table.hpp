651fae1ec295c3279047a6e77ba33fe71899f19f
Felipe Graeff <felipe.graeff@inf.ufrgs.br>
2017-07-19 01:18:28 +0000

    relatorio

--- lib/hash_table.hpp
+++ lib/hash_table.hpp
@@ -13,71 +13,69 @@
 
 namespace cpd{
 
-template <typename K, typename T> class HashIter;
+template <typename K, typename T>
+class HashIter;
 
 //===============HashTable===============
 template <typename K, typename T>
 class HashTable{
-  private:
-    typedef std::vector<std::list<std::pair<K,T> > > Table;
-    typedef std::list<std::pair<K,T> > Bucket;
-    const int initialSize = 17;
-    Table table;
-    int tableSize;
-    int occupancy;
+ private:
+  typedef std::vector<std::list<std::pair<K,T> > > Table;
+  typedef std::list<std::pair<K,T> > Bucket;
+  const int initialSize = 17;
+  Table table;
+  int tableSize;
+  int occupancy;
 
-    void resize();
-    void shrinkToFit();
-    // hash functions for supported types
+  void resize();
+  void shrinkToFit();
+  // hash functions for supported types
   unsigned long hash(K value);
-    //unsigned long hash(std::string value);
-    //unsigned long hash(int value);
-  public:
-    // iterator definitions
-    friend class HashIter<K,T>;
-    typedef HashIter<K,T> iterator;
-    typedef ptrdiff_t difference_type;
-    typedef size_t size_type;
-    typedef T value_type;
-    typedef T* pointer;
-    typedef T& reference;
+  //unsigned long hash(std::string value);
+  //unsigned long hash(int value);
+ public:
+  // iterator definitions
+  friend class HashIter<K,T>;
+  typedef HashIter<K,T> iterator;
+  typedef ptrdiff_t difference_type;
+  typedef size_t size_type;
+  typedef T value_type;
+  typedef T* pointer;
+  typedef T& reference;
 
-    // member functions
-    HashTable();
-    int getSize();
-    int getOccupancy();
-    int getCollisions();
-    void insert(const K key, const T item);
-    void remove(const iterator item);
-    iterator search(const K key);
-    iterator begin();
-    iterator end();
+  // member functions
+  HashTable();
+  int getSize();
+  int getOccupancy();
+  int getCollisions();
+  void insert(const K key, const T item);
+  void remove(const iterator item);
+  iterator search(const K key);
+  iterator begin();
+  iterator end();
 };
 
   //===============Iterator===============
 template <typename K, typename T>
 class HashIter{
-  private:
-    HashTable<K,T>& hashTable;
-    typename HashTable<K,T>::Table::iterator curBucket;
-    typename HashTable<K,T>::Bucket::iterator curItem;
-  public:
-    HashIter(HashTable<K,T>& ht, bool isBegin);
-    HashIter(HashTable<K,T>& ht,
-      typename HashTable<K,T>::Table::iterator bucket,
-      typename HashTable<K,T>::Bucket::iterator item);
-    HashIter(const HashIter<K,T>& hi); // copy constructor
-    typename HashTable<K,T>::Table::iterator getCurBucket();
-    typename HashTable<K,T>::Bucket::iterator getCurItem();
-    bool operator==(const HashIter<K,T>& other);
-    bool operator!=(const HashIter<K,T>& other);
-    T& operator*();
-    HashIter<K,T>& operator++(); // prefix: ++iter
-    HashIter<K,T> operator++ (int); //postfix: iter++
+ private:
+  HashTable<K,T>& hashTable;
+  typename HashTable<K,T>::Table::iterator curBucket;
+  typename HashTable<K,T>::Bucket::iterator curItem;
+ public:
+  HashIter(HashTable<K,T>& ht, bool isBegin);
+  HashIter(HashTable<K,T>& ht,
+           typename HashTable<K,T>::Table::iterator bucket,
+           typename HashTable<K,T>::Bucket::iterator item);
+  HashIter(const HashIter<K,T>& hi); // copy constructor
+  typename HashTable<K,T>::Table::iterator getCurBucket();
+  typename HashTable<K,T>::Bucket::iterator getCurItem();
+  bool operator==(const HashIter<K,T>& other);
+  bool operator!=(const HashIter<K,T>& other);
+  T& operator*();
+  HashIter<K,T>& operator++(); // prefix: ++iter
+  HashIter<K,T> operator++ (int); //postfix: iter++
 };
 }  // namespace cpd
-
-// template class cpd::HashTable<int,Review>;
-// template class cpd::HashTable<std::string,Word>;
 
 #endif
